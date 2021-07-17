53dbed6f9e53ee880bd18299d15f3e2e1dc9a921
Jerome Gacoscosim <40538528+jeromeski@users.noreply.github.com>
2021-07-17 12:25:44 +0800

    Intial loading of users

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import { StrictMode } from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
+import { StrictMode } from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 
-import App from "./App";
-import store from "./redux/store";
+import App from './App';
+import store from './redux/store';
 
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById('root');
 ReactDOM.render(
   <StrictMode>
     <Provider store={store}>
